da5dde213dce2a153105bc8d4fb590887789248c
dhillon1 <simrandhillon5486@gmail.com>
2020-04-17 04:29:13 -0400

    Add working fine and label as well in the game of unity

---
+++ Assets/_Scripts/BasketController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class BasketController : MonoBehaviour
+{
+    public Camera camera;
+    public AudioSource music;
+
+    public Text Live;
+    public Text Score;
+
+    public int _live = 4;
+    public int _score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2 (Mathf.Clamp(mousePosition.x,-1.48f,1.48f),Mathf.Clamp(mousePosition.y,-4f,1.38f));
+    }
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        music.Play();
+        _score  = _score + 10;
+        Score.text = "Score : " + _score.ToString(); 
+
+        
+    }
+
+}
---
+++ Assets/_Scripts/EggController2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggController2 : MonoBehaviour
+{
+      // Start is called before the first frame update
+    public float verticalSpeed;
+
+    public BasketController basketController;
+    void Start()
+    {
+        verticalSpeed = Random.Range(0.019f, 0.078f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y <= -4.7f)
+        {
+            basketController._live = basketController._live - 1;
+            basketController.Live.text = "Live: "  + basketController._live.ToString();
+            Start();
+            transform.position = new Vector3(0.0f,2.3f,0.0f);
+        }
+
+
+        transform.position -= new Vector3(0.0f,verticalSpeed,0.0f);
+        
+    }
+
+    public void reset(){
+        Start();
+        transform.position = new Vector3(0.0f,2.3f,0.0f);
+
+    }
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        reset();
+    }
+}
